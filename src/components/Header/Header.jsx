3a0df21655c0a64f40d9a6a9a4dbfaa1b3211b10
Athif Binu <binuathif@gmail.com>
2023-08-16 17:19:21 +0530

    add badge

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,42 +1,40 @@
-import React, { useState } from 'react';
-import "./Header.css"
-import logo from "../../assets/empty (1).png"
-import india from "../../assets/india.png"
-import {BiComment} from "react-icons/bi"
-import {GoPerson} from "react-icons/go"
-import {MdArrowForwardIos,MdArrowDropDown} from "react-icons/md"
+import React, { useState } from "react";
+import "./Header.css";
+import logo from "../../assets/empty (1).png";
+import india from "../../assets/india.png";
+import { BiComment } from "react-icons/bi";
+import { GoPerson } from "react-icons/go";
+import { MdArrowForwardIos, MdArrowDropDown } from "react-icons/md";
 
 const Header = () => {
-
-
-
   return (
-    <header className='flex'>
-      <div className="logo-section flex">
-        <img  src={logo} alt="" />
-    
-        <p>HOME</p>
-        <MdArrowForwardIos/>
-        <p>CONTRACT</p>
-        <MdArrowForwardIos/>
-        <p>TRAVEL CERTIFICATE</p>
-        <MdArrowForwardIos/>
-         <p>ADD TRAVEL CERTIFICATE</p>
+    <header className="flex">
+    <div className="logo-section flex">
+      <img src={logo} alt="" />
+
+      <p>HOME</p>
+      <MdArrowForwardIos />
+      <p>CONTRACT</p>
+      <MdArrowForwardIos />
+      <p>TRAVEL CERTIFICATE</p>
+      <MdArrowForwardIos />
+      <p>ADD TRAVEL CERTIFICATE</p>
+    </div>
+
+    <div className="last-section flex">
+      <h3>RTK</h3>
+      <img src={india} alt="" />
+      <MdArrowDropDown />
+      <hr style={{ height: "18px" }} />
+
+      <div className="message-icon">
+        <BiComment size={27} />
+        <span class="badge">5</span>
       </div>
-
-
-      <div className="last-section flex">
-          <h3>RTK</h3>
-          <img src={india} alt="" />
-           <MdArrowDropDown/>
-           <hr style={{height:"18px"}}/>
-          <BiComment size={18}/>
-          <GoPerson size={18}/>
-      </div>
-
-
-    </header>
+      <GoPerson size={27} />
+    </div>
+  </header>
   );
-}
+};
 
 export default Header;
